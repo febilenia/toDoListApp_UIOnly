cfa3c12a023522833e8ffd974f78aeddba126828
Febilenia <febileniarismapa@gmail.com>
2020-10-12 08:18:39 +0700

    Initial commit

---
+++ toDoListApp_UIOnly-master/app/src/main/java/com/example/todolistapp_ui_only/Adapter/ToDoAdapter.java
@@ -0,0 +1,61 @@
+package com.example.todolistapp_ui_only.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.todolistapp_ui_only.MainActivity;
+import com.example.todolistapp_ui_only.Model.ToDoModel;
+import com.example.todolistapp_ui_only.R;
+
+import java.util.List;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
+
+    private List<ToDoModel> todoList;
+    private MainActivity activity;
+
+    public ToDoAdapter(MainActivity activity){
+        this.activity = activity;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_layout, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position){
+        ToDoModel item = todoList.get(position);
+        holder.task.setText(item.getTask());
+        holder.task.setChecked(toBoolean(item.getStatus()));
+    }
+
+    private boolean toBoolean(int n){
+        return n!=0;
+    }
+
+    @Override
+    public int getItemCount(){
+        return todoList.size();
+    }
+
+    public void setTasks(List<ToDoModel> todoList){
+        this.todoList = todoList;
+        notifyDataSetChanged();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        CheckBox task;
+
+        ViewHolder(View view){
+            super(view);
+            task = view.findViewById(R.id.todoCheckBox);
+        }
+    }
+}
---
+++ toDoListApp_UIOnly-master/app/src/main/java/com/example/todolistapp_ui_only/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.todolistapp_ui_only;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.todolistapp_ui_only.Adapter.ToDoAdapter;
+import com.example.todolistapp_ui_only.Model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView tasksRecycleView;
+    private ToDoAdapter tasksAdapter;
+
+    private List<ToDoModel> taskList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        taskList = new ArrayList<>();
+
+        tasksRecycleView = findViewById(R.id.tasksRecyclerview);
+        tasksRecycleView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecycleView.setAdapter(tasksAdapter);
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("This is my to do list!");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+
+        tasksAdapter.setTasks(taskList);
+
+
+
+    }
+}
